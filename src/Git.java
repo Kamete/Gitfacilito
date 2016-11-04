f1f2b7120c6d0c318410cfb0206a12c4d225d046
Miguel <miguel_ffx2@hotmail.com>
2016-11-04 20:50:11 +0100

    iniciamos

---
+++ src/Git.java
@@ -0,0 +1,12 @@
+/**
+ * Created by migue on 04/11/2016.
+ */
+public class Git {
+    public static void main(String[] args) {
+        printHola();
+    }
+
+    public static void printHola() {
+        System.out.println("Hello world!");
+    }
+}
